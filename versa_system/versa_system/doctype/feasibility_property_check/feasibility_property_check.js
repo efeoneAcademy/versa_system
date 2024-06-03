ec4d0a82d10cd8ca73609dd229aa4fad268d7c7b
Thesneema M <thezniz123@gmail.com>
2024-06-03 16:39:20 +0530

    fix: Renamed DocType to Feasibility Property Check

--- versa_system/versa_system/doctype/feasibility_property_check/feasibility_property_check.js
+++ versa_system/versa_system/doctype/feasibility_property_check/feasibility_property_check.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, efeone and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Fesibility Property Check", {
+// frappe.ui.form.on("Feasibility Property Check", {
 // 	refresh(frm) {
 
 // 	},
--- versa_system/versa_system/doctype/feasibility_property_check/test_feasibility_property_check.py
+++ versa_system/versa_system/doctype/feasibility_property_check/test_feasibility_property_check.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestFesibilityPropertyCheck(FrappeTestCase):
+class TestFeasibilityPropertyCheck(FrappeTestCase):
 	pass

cfce31bac49574eccff5a19e9ec24e5f516e5c82
Nidha Shirin <nidhashirin34@gmail.com>
2024-06-04 16:42:18 +0530

    feat: New Feature Button From Lead To Quotation Form

--- versa_system/hooks.py
+++ versa_system/hooks.py
@@ -28,7 +28,7 @@ app_license = "mit"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-# doctype_js = {"doctype" : "public/js/doctype.js"}
+doctype_js = {"Lead" : "public/js/lead.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
@@ -232,7 +232,6 @@ app_license = "mit"
 fixtures = [
     {"dt":"Role", "filters":[["name", "=", "Manufacturing  User"]]},
     {"dt":"Workflow","filters":[["name","in",["Feasibility", "Quotation Approval", "Mockup Design Approval"]]]},
-    {"dt":"Property Setter", "filters":[["module", "=", "Versa System"]]},
     {"dt":"Workflow State","filters":[["name","in",["sent to customer"]]]},
     {"dt":"Workflow Action Master","filters":[["name","in",["send to customer"]]]},
 ]

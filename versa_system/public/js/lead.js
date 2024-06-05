5ac60ffa2aae065338526afbce6dbe4794076348
Neha Fathima <nehaputhur443@gmail.com>
2024-06-05 17:12:39 +0530

    fix: Changed Buttons to only Map data and not Create

--- versa_system/public/js/lead.js
+++ versa_system/public/js/lead.js
@@ -1,12 +1,6 @@
 
 frappe.ui.form.on('Lead', {
   refresh: function(frm) {
-    frm.add_custom_button(__('New Quotation'), function() {
-      frappe.model.open_mapped_doc({
-          method : 'versa_system.versa_system.custom_scripts.lead.lead.map_lead_to_quotation',
-          frm : frm
-        });
-    }, __('Create'));
 
     setTimeout(() => {
       frm.remove_custom_button('Quotation', 'Create');
@@ -15,19 +9,26 @@ frappe.ui.form.on('Lead', {
       frm.remove_custom_button('Opportunity', 'Create');
     }, 10);
 
-    frm.add_custom_button(__("Feasibility Property Check"), function() {
-        frappe.call({
-            method: "versa_system.versa_system.custom_scripts.lead.lead.map_lead_to_feasibility_check",
-            args: {
-                source_name: frm.doc.name
-            },
-            callback: function(r) {
-                if (r.message) {
-                    frappe.set_route("Form", "Feasibility Property Check", r.message);
-                }
-            }
+    frm.add_custom_button(__('New Quotation'), function() {
+      frappe.model.open_mapped_doc({
+          method : 'versa_system.versa_system.custom_scripts.lead.lead.map_lead_to_quotation',
+          frm : frm
         });
-    }, __("Create"));
+    }, __('Create'));
+
+    frm.add_custom_button(__('Feasibility Property Check'), function() {
+      frappe.model.open_mapped_doc({
+          method : 'versa_system.versa_system.custom_scripts.lead.lead.map_lead_to_feasibility_check',
+          frm : frm
+        });
+    }, __('Create'));
+
+    frm.add_custom_button(__('Mockup Design'), function() {
+      frappe.model.open_mapped_doc({
+          method : 'versa_system.versa_system.custom_scripts.lead.lead.map_lead_to_mockup_design',
+          frm : frm
+        });
+    }, __('Create'));
 
   }
 });

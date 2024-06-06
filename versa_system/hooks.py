36795571fb6ec0a42a302355cfc27c31b9536342
RASNA96 <rasnaravindran654@gmail.com>
2024-06-06 16:28:13 +0530

    feat: Add Final Design button to Approved Quotation

--- versa_system/hooks.py
+++ versa_system/hooks.py
@@ -28,7 +28,7 @@ app_license = "mit"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-doctype_js = {"Lead" : "public/js/lead.js"}
+doctype_js = {"Lead" : "public/js/lead.js","Quotation" : "public/js/quotation.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}

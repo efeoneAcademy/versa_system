36aad4d8687e35d545fdf113571456cae110d55e
hridyalakshmi <hridyalkrishnan2001@gmail.com>
2024-06-07 15:29:38 +0530

    feat:validated Lead Form

--- versa_system/public/js/lead.js
+++ versa_system/public/js/lead.js
@@ -1,7 +1,6 @@
 
 frappe.ui.form.on('Lead', {
   refresh: function(frm) {
-
     setTimeout(() => {
       frm.remove_custom_button('Quotation', 'Create');
       frm.remove_custom_button('Customer', 'Create');
@@ -9,26 +8,52 @@ frappe.ui.form.on('Lead', {
       frm.remove_custom_button('Opportunity', 'Create');
     }, 10);
 
-    frm.add_custom_button(__('New Quotation'), function() {
+
+    let quotationButton=frm.add_custom_button(__('New Quotation'), function() {
       frappe.model.open_mapped_doc({
           method : 'versa_system.versa_system.custom_scripts.lead.lead.map_lead_to_quotation',
           frm : frm
         });
     }, __('Create'));
 
-    frm.add_custom_button(__('Feasibility Property Check'), function() {
+
+
+     let feasibilityCheckButton=frm.add_custom_button(__('Feasibility Property Check'), function() {
       frappe.model.open_mapped_doc({
           method : 'versa_system.versa_system.custom_scripts.lead.lead.map_lead_to_feasibility_check',
           frm : frm
         });
     }, __('Create'));
 
-    frm.add_custom_button(__('Mockup Design'), function() {
+      let mockupDesignButton=frm.add_custom_button(__('Mockup Design'), function() {
       frappe.model.open_mapped_doc({
           method : 'versa_system.versa_system.custom_scripts.lead.lead.map_lead_to_mockup_design',
           frm : frm
         });
     }, __('Create'));
+
+
+    if (frm.doc.status === "Mockup Design Approved") {
+            mockupDesignButton.hide();
+            quotationButton.show();
+            feasibilityCheckButton.hide();
+        } else if(frm.doc.status === "Mockup Design Rejected"){
+            mockupDesignButton.hide();
+            quotationButton.hide();
+            feasibilityCheckButton.hide();
+        }else if (frm.doc.status === "Mockup Design Pending") {
+            mockupDesignButton.hide();
+            quotationButton.hide();
+            feasibilityCheckButton.hide();
+
+        }else {
+              // Default case: hide all buttons
+              quotationButton.hide();
+              mockupDesignButton.show();
+              feasibilityCheckButton.show();
+          }
+
+
 
     set_model_query(frm)
 

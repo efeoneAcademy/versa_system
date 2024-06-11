f1aab759da67935b0f9f8a07ab5db5254b8e2f69
RASNA96 <rasnaravindran654@gmail.com>
2024-06-11 15:06:00 +0530

    feat: Add Final Design button to Approved Quotation

--- versa_system/public/js/lead.js
+++ versa_system/public/js/lead.js
@@ -6,56 +6,63 @@ frappe.ui.form.on('Lead', {
       frm.remove_custom_button('Prospect', 'Create');
       frm.remove_custom_button('Opportunity', 'Create');
 
-      let quotation_button=frm.add_custom_button(__('New Quotation'), function() {
+      let quotation_button = frm.add_custom_button(__('New Quotation'), function() {
         frappe.model.open_mapped_doc({
-            method : 'versa_system.versa_system.custom_scripts.lead.lead.map_lead_to_quotation',
-            frm : frm
-          });
+          method: 'versa_system.versa_system.custom_scripts.lead.lead.map_lead_to_quotation',
+          frm: frm
+        });
       }, __('Create'));
 
-       let feasibility_check_button=frm.add_custom_button(__('Feasibility Property Check'), function() {
+      let feasibility_check_button = frm.add_custom_button(__('Feasibility Property Check'), function() {
         frappe.model.open_mapped_doc({
-            method : 'versa_system.versa_system.custom_scripts.lead.lead.map_lead_to_feasibility_check',
-            frm : frm
-          });
+          method: 'versa_system.versa_system.custom_scripts.lead.lead.map_lead_to_feasibility_check',
+          frm: frm
+        });
       }, __('Create'));
 
-        let mockup_design_button=frm.add_custom_button(__('Mockup Design'), function() {
+      let mockup_design_button = frm.add_custom_button(__('Mockup Design'), function() {
         frappe.model.open_mapped_doc({
-            method : 'versa_system.versa_system.custom_scripts.lead.lead.map_lead_to_mockup_design',
-            frm : frm
-          });
+          method: 'versa_system.versa_system.custom_scripts.lead.lead.map_lead_to_mockup_design',
+          frm: frm
+        });
       }, __('Create'));
 
-         // Initialize all buttons to hidden
-         mockup_design_button.hide();
-         quotation_button.hide();
-         feasibility_check_button.hide();
+      let final_design_button = frm.add_custom_button(__('Final Design'), function() {
+        frappe.model.open_mapped_doc({
+          method: 'versa_system.versa_system.custom_scripts.lead.lead.map_lead_to_final_design',
+          frm: frm
+        });
+      }, __('Create'));
 
-         // Handle Lead buttons visibility based on status
-         if (frm.doc.status === "Mockup Design Approved") {
-           quotation_button.show();
-         } else if (frm.doc.status === "Mockup Design Rejected") {
-           // All buttons remain hidden
-         } else if (frm.doc.status === "Mockup Design Pending") {
-            mockup_design_button.show();
-         } else if (frm.doc.status === "Lead") {
-            feasibility_check_button.show();
-         } else if (frm.doc.status === "Feasibility Check Approved") {
-           mockup_design_button.show();
-           quotation_button.show();
-         } else if (frm.doc.status === "Feasibility Check Rejected") {
-           // All buttons remain hidden
-         } else {
-           // Default case for any other status
-            mockup_design_button.show();
-            feasibility_check_button.show();
-          }
+      // Initialize all buttons to hidden
+      mockup_design_button.hide();
+      quotation_button.hide();
+      feasibility_check_button.hide();
+      final_design_button.hide();
 
+      // Handle Lead buttons visibility based on status
+      if (frm.doc.status === "Mockup Design Approved") {
+        quotation_button.show();
+      } else if (frm.doc.status === "Mockup Design Rejected") {
+        // All buttons remain hidden
+      } else if (frm.doc.status === "Mockup Design Pending") {
+        mockup_design_button.show();
+      } else if (frm.doc.status === "Lead") {
+        feasibility_check_button.show();
+      } else if (frm.doc.status === "Feasibility Check Approved") {
+        mockup_design_button.show();
+        quotation_button.show();
+      } else if (frm.doc.status === "Feasibility Check Rejected") {
+        // All buttons remain hidden
+      } else if (frm.doc.status === "Quotation") {
+        final_design_button.show();
+      } else {
+        // Default case for any other status
+        mockup_design_button.show();
+        feasibility_check_button.show();
+      }
 
-
-
-      set_model_query(frm)
+      set_model_query(frm);
 
     }, 10);
   }
@@ -63,20 +70,20 @@ frappe.ui.form.on('Lead', {
 
 frappe.ui.form.on("Properties Table", {
   item_type: function(frm, cdt, cdn) {
-    set_model_query(frm)
+    set_model_query(frm);
   }
-})
+});
 
 function set_model_query(frm) {
   /*
   * Function sets filter on Model field in Properties Table based on selected Item Type
   */
   frm.fields_dict["custom_property_table"].grid.get_field("model").get_query = function(doc, cdt, cdn) {
-      var child = locals[cdt][cdn];
-      return {
-          filters:[
-              ["item_type", "=", child.item_type]
-          ]
-      }
+    var child = locals[cdt][cdn];
+    return {
+      filters: [
+        ["item_type", "=", child.item_type]
+      ]
+    };
   }
 }

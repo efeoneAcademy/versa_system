4cf6cd714d98de7f5f9746fb982e29ab4b0b9955
RASNA96 <rasnaravindran654@gmail.com>
2024-06-11 15:06:00 +0530

    feat:Add Final Design button to fetch data from Lead and Quotation

--- versa_system/versa_system/custom_scripts/lead/lead.py
+++ versa_system/versa_system/custom_scripts/lead/lead.py
@@ -10,28 +10,26 @@ def map_lead_to_quotation(source_name, target_doc=None):
     '''
     def set_missing_values(source, target):
         target.quotation_to = "Lead"
+
     target_doc = get_mapped_doc("Lead", source_name,
         {
             "Lead": {
                 "doctype": "Quotation",
                 "field_map": {
-                    "name":"party_name"
+                    "name": "party_name"
                 },
             },
-
         }, target_doc, set_missing_values)
     return target_doc
 
 @frappe.whitelist()
 def map_lead_to_feasibility_check(source_name, target_doc=None):
     '''
-        Method : Method to map a Lead to a Feasibility Property Check document
-
-        Args :
-         source_name : The name of the Lead document to be mapped
-         target_doc : The target document to which the Lead and its Properties Table should be mapped.
-
-        Output :  Name of the newly created Feasibility Property Check document.
+        Method: Method to map a Lead to a Feasibility Property Check document
+        Args:
+         source_name: The name of the Lead document to be mapped
+         target_doc: The target document to which the Lead and its Properties Table should be mapped.
+        Output: Name of the newly created Feasibility Property Check document.
     '''
     def set_missing_values(source, target):
         pass
@@ -61,8 +59,8 @@ def map_lead_to_feasibility_check(source_name, target_doc=None):
 
 @frappe.whitelist()
 def map_lead_to_mockup_design(source_name, target_doc=None):
-    """method to get Mockup Design for custom button in lead doctype by using mapdoc.
-       output: data from lead is mapped to a new mockup design document
+    """Method to get Mockup Design for custom button in lead doctype by using mapdoc.
+       Output: data from lead is mapped to a new mockup design document
     """
     def set_missing_values(source, target):
         pass
@@ -74,26 +72,26 @@ def map_lead_to_mockup_design(source_name, target_doc=None):
             "field_map": {
             },
         },
-         "Properties Table": {
-                "doctype": "Properties Table",
-                 "field_map": {
-                    'item_type': 'item_type',
-                    'meterial_type': 'meterial_type',
-                    'design': 'design',
-                    'model': 'model',
-                    'brand': 'brand',
-                    'size_chart': 'size_chart',
-                    'rate_range': 'rate_range'
-                },
+        "Properties Table": {
+            "doctype": "Properties Table",
+            "field_map": {
+                'item_type': 'item_type',
+                'meterial_type': 'meterial_type',
+                'design': 'design',
+                'model': 'model',
+                'brand': 'brand',
+                'size_chart': 'size_chart',
+                'rate_range': 'rate_range'
             },
+        },
     }, target_doc, set_missing_values)
 
     return target_doc
 
 @frappe.whitelist()
 def get_lead_properties(lead_name):
-    """method to  Get data from the Lead in to child table in Mockup Design.
-    output: data from lead is mapped to a new mockup design document
+    """Method to get data from the Lead into child table in Mockup Design.
+       Output: data from lead is mapped to a new mockup design document
     """
     if frappe.db.exists("Lead", lead_name):
         lead = frappe.get_doc("Lead", lead_name)
@@ -111,3 +109,50 @@ def get_lead_properties(lead_name):
         return custom_property_table
     else:
         frappe.throw(_("Lead not found"))
+
+
+@frappe.whitelist()
+def map_lead_to_final_design(source_name, target_doc=None):
+    '''
+        Method: Method to map Lead information to Final Design document.
+
+        Output: Updated Final Design document.
+    '''
+    def set_missing_values(source, target):
+        '''
+        Method: Sets missing values from Quotation items to the target Final Design document.
+
+        Output: None
+    '''
+        quotation = frappe.get_all("Quotation", filters={"party_name": source_name}, fields=["name"])
+        if quotation:
+            quotation_doc = frappe.get_doc("Quotation", quotation[0].name)
+            for item in quotation_doc.items:
+                target.append('quotation_items', {
+                    'item_code': item.item_code,
+                    'item_group': item.item_group,
+                    'stock_uom': item.stock_uom,
+                })
+
+    target_doc = get_mapped_doc("Lead", source_name,
+        {
+            "Lead": {
+                "doctype": "Final Design",
+                "field_map": {
+                },
+            },
+            "Properties Table": {
+                "doctype": "Properties Table",
+                "field_map": {
+                    'item_type': 'item_type',
+                    'material_type': 'material_type',
+                    'design': 'design',
+                    'model': 'model',
+                    'brand': 'brand',
+                    'size_chart': 'size_chart',
+                    'rate_range': 'rate_range'
+                },
+            },
+        }, target_doc, set_missing_values)
+
+    return target_doc

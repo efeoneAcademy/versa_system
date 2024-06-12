2161bb9008565daba8797b598859ac1e991042ed
Nidha Shirin <nidhashirin34@gmail.com>
2024-06-12 13:22:23 +0530

    fix : Adding Copyright

--- versa_system/versa_system/doctype/size_chart/size_chart.js
+++ versa_system/versa_system/doctype/size_chart/size_chart.js
@@ -1,12 +1,5 @@
-
 // Copyright (c) 2024, efeone and contributors
 // For license information, please see license.txt
-
-// frappe.ui.form.on("Size Chart", {
-// 	refresh(frm) {
-
-// 	},
-// });
 
 frappe.ui.form.on('Size Chart', {
     onload: function(frm) {

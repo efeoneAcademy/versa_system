032687ebad1c1937f06512eb6e6fb0c9cbbdb6da
asmakv <asmakv972@gmail.com>
2024-06-13 16:46:51 +0530

    feat: automatic calculation in the raw material bundle childtable

--- versa_system/versa_system/doctype/raw_material_bundle/raw_material_bundle.js
+++ versa_system/versa_system/doctype/raw_material_bundle/raw_material_bundle.js
@@ -5,11 +5,25 @@ frappe.ui.form.on('Raw Material Bundle', {
     onload: function(frm) {
         frm.set_query('reference_doctype', function() {
             return {
-              "filters":{
-                module: 'Versa System'
-              }
-
+                "filters": {
+                    module: 'Versa System'
+                }
             };
         });
+    }
+});
+
+frappe.ui.form.on('Raw Material', {
+    quantity: function(frm, cdt, cdn) {
+        let d = locals[cdt][cdn];
+        let amt = d.quantity * d.rate;
+        frappe.model.set_value(cdt, cdn, 'total_amount', amt);
+        frm.refresh_field('raw_material'); // Refresh the raw_material field
+    },
+    rate: function(frm, cdt, cdn) {
+        let d = locals[cdt][cdn];
+        let amt = d.quantity * d.rate;
+        frappe.model.set_value(cdt, cdn, 'total_amount', amt);
+        frm.refresh_field('raw_material'); // Refresh the raw_material field
     }
 });

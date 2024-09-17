f23b47f6843ee521763adf09953934a969b18b03
Ajmal Roshan <ajmalroshan2002@gmail.com>
2024-09-17 14:45:33 +0530

    feat:Add button to map the data from Lead to feasibility check

--- versa_system/versa_system/custom_scripts/lead.py
+++ versa_system/versa_system/custom_scripts/lead.py
@@ -3,31 +3,39 @@ from frappe.model.mapper import get_mapped_doc
 
 @frappe.whitelist()
 def map_lead_to_feasibility_check(source_name, target_doc=None):
+    """
+    Map fields from the Lead DocType to Feasibility Check DocType.
+    
+    Parameters:
+        source_name (str): The name of the Lead document to be mapped.
+        target_doc (Document, optional): An existing target document to map to. Defaults to None.
+    
+    Returns:
+        Document: The mapped target document.
+    """
 
+    # Use get_mapped_doc to map fields from Lead to Feasibility Check and related DocTypes
     target_doc = get_mapped_doc("Lead", source_name,
         {
             "Lead": {
                 "doctype": "Feasibility Check",
                 "field_map": {
-    
-                    "first_name": "from_lead",              
-                    #"custom_material_type": "material"
+                    "first_name": "from_lead"
                 },
             },
             "Properties": {                               
                 "doctype": "Properties",
                 "field_map": {
-                    'finishing': 'finishing',             
-                    'color': 'color',
-                    'material': 'material'
+                    'finishing': 'finishing',  
+                    'color': 'color',          
+                    'material': 'material'     
                 },
             },
             "Size Chart": {                               
                 "doctype": "Size Chart",
                 "field_map": {
-                    'size': 'size',             
-                    'dimensions': 'dimensions',
-                    
+                    'size': 'size',          
+                    'dimensions': 'dimensions' 
                 },
             },
         }, target_doc)

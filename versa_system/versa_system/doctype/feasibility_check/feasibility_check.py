144ed25d963604646f2e4ae8cf0f8f3728c6687c
Ajmal Roshan <ajmalroshan2002@gmail.com>
2024-09-18 17:00:33 +0530

    feat: create moc_design based on workflow

--- versa_system/versa_system/doctype/feasibility_check/feasibility_check.py
+++ versa_system/versa_system/doctype/feasibility_check/feasibility_check.py
@@ -1,9 +1,15 @@
-# Copyright (c) 2024, efeone and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class FeasibilityCheck(Document):
-	pass
+    def on_update(self):
+        print(self.workflow_state)
+        if self.workflow_state == 'Approved':
+                    moc_design = frappe.get_doc({
+                        'doctype': 'Mockup Design',
+                        'from_lead': self.from_lead
+                    })
+                    
+                    moc_design.insert(ignore_permissions=True)
+                    frappe.msgprint(f'Mockup Design {moc_design.from_lead} created successfully.')
+                

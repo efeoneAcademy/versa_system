1d63f557ddb02755e62f88bcde8cf6bb79834f44
NaseeraVP <naseeravp38@gmail.com>
2024-09-23 13:10:18 +0530

    feat:created a button from lead to quotation

--- versa_system/public/js/lead.js
+++ versa_system/public/js/lead.js
@@ -6,5 +6,19 @@ frappe.ui.form.on('Lead', {
                 frm: frm
             });
         }, __('Create'));
+
+        frm.add_custom_button(__('New Quotation'), function() {
+            frappe.model.open_mapped_doc({
+                method: 'versa_system.versa_system.custom_scripts.lead.map_lead_to_quotation',
+                frm: frm
+            });
+        }, __('Create'));
+
+        setTimeout(() => {
+            frm.remove_custom_button('Quotation', 'Create');
+            frm.remove_custom_button('Customer', 'Create');
+            frm.remove_custom_button('Prospect', 'Create');
+            frm.remove_custom_button('Opportunity', 'Create');
+        }, 10);
     }
 });

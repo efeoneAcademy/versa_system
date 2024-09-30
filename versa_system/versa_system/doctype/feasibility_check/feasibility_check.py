415551f23f6305dc71f79b9f8d17e15aaaff2bf3
NaseeraVP <naseeravp38@gmail.com>
2024-09-30 16:02:51 +0530

    Fix the lead doctype create button is visible after the lead is saved

--- versa_system/versa_system/doctype/feasibility_check/feasibility_check.py
+++ versa_system/versa_system/doctype/feasibility_check/feasibility_check.py
@@ -4,13 +4,15 @@ from frappe.model.document import Document
 class FeasibilityCheck(Document):
 
     def on_update(self):
-        "IF the feasibility Check is Approved then the moc design will be created "
+        "IF the feasibility Check is Approved then the mockup design will be created "
         if self.workflow_state == 'Approved':
-                    moc_design = frappe.get_doc({
-                        'doctype': 'Mockup Design',
-                        'from_lead': self.from_lead
-                    })
-                    
-                    moc_design.insert(ignore_permissions=True)
-                    frappe.msgprint(f'Mockup Design {moc_design.from_lead} created successfully.')
-                
+            # Create the Mockup Design document
+            moc_design = frappe.get_doc({
+                'doctype': 'Mockup Design',
+                'from_lead': self.from_lead,
+                'material': self.material  # Fetching material data from FeasibilityCheck
+            })
+
+            # Insert the new Mockup Design document
+            moc_design.insert(ignore_permissions=True)
+            frappe.msgprint(f'Mockup Design {moc_design.from_lead} created successfully.')

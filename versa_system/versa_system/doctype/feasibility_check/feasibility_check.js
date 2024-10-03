84188fd49f91c0e599595dd44b50f0bc8567a5b9
Sreelakshmi1831 <sreelakshmitp282@gmail.com>
2024-10-03 10:05:26 +0530

    feat created a button to mockup design

--- versa_system/versa_system/doctype/feasibility_check/feasibility_check.js
+++ versa_system/versa_system/doctype/feasibility_check/feasibility_check.js
@@ -1,8 +1,23 @@
-// Copyright (c) 2024, efeone and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Feasibility Check", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Feasibility Check', {
+    refresh: function(frm) {
+        if (frm.doc.workflow_state === 'Approved') {
+            frm.add_custom_button(__('Mockup Design'), function() {
+                frappe.call({
+                    method: 'frappe.client.insert',
+                    args: {
+                        doc: {
+                            doctype: 'Mockup Design',
+                            from_lead: frm.doc.from_lead
+                        }
+                    },
+                    callback: function(r) {
+                        if (r.message) {
+                            // Redirect to the newly created Mockup Design
+                            frappe.set_route('Form', 'Mockup Design', r.message.name);
+                        }
+                    }
+                });
+            });
+        }
+    }
+});

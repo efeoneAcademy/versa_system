71f9253b29c0612043abe372b15e129e6295902e
NaseeraVP <naseeravp38@gmail.com>
2024-10-03 14:18:08 +0530

    fix:Resolved Conflict

--- versa_system/hooks.py
+++ versa_system/hooks.py
@@ -30,6 +30,7 @@ app_license = "mit"
 # include js in doctype views
 doctype_js = {"Lead" : "public/js/lead.js","Quotation" : "public/js/quotation.js"}
 
+
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}

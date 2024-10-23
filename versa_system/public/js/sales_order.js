37cf97cd357e53cf2023b17e0d82385d7a5d9cb9
hanihassan01 <hani77327@gmail.com>
2024-10-23 15:29:42 +0530

    fDat: default delivery date in the Sales Order to be 10 days from the currate

---
+++ versa_system/public/js/sales_order.js
@@ -0,0 +1,15 @@
+frappe.ui.form.on("Sales Order", {
+  onload: function (frm) {
+    if (!frm.doc.delivery_date) {
+      // Check if delivery_date is not already set
+      // Get today's date
+      var today = new Date();
+      // Add 10 days to today's date
+      today.setDate(today.getDate() + 10);
+      // Format the date to YYYY-MM-DD
+      var defaultDate = today.toISOString().split("T")[0];
+      // Set the default date in the delivery_date field
+      frm.set_value("delivery_date", defaultDate);
+    }
+  },
+});

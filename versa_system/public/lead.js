dc0de07346bb576eb8c706a9abd6cc3917bafa9d
anshidakk <anshida41@gmail.com>
2024-12-18 10:43:28 +0530

    feat:add new button in lead and fetch the details in lead in to quotation

--- versa_system/public/lead.js
+++ versa_system/public/lead.js
@@ -1,5 +1,5 @@
 frappe.ui.form.on('Lead', {
-    refresh: function (frm) {
+    refresh: function(frm) {
         // Filter for custom item details
         frm.fields_dict['custom_item_details'].grid.get_field('item').get_query = function(doc, cdt, cdn) {
             return {
@@ -8,6 +8,7 @@ frappe.ui.form.on('Lead', {
                 ]
             };
         };
+
         frm.fields_dict['custom_item_details'].grid.get_field('material').get_query = function(doc, cdt, cdn) {
             return {
                 filters: [
@@ -16,17 +17,27 @@ frappe.ui.form.on('Lead', {
             };
         };
 
+        // Add "Feasibility Check" button
         frm.add_custom_button(
-          __("Feasibility Check"),
-          function () {
-            frappe.model.open_mapped_doc({
-              method:
-                "versa_system.versa_system.doctype.feasibility_check.feasibility_check.map_lead_to_feasibility_check",
-              frm: frm,
-            });
-          },
-          __("Create")
+            __("Feasibility Check"),
+            function () {
+                frappe.model.open_mapped_doc({
+                    method: "versa_system.versa_system.doctype.feasibility_check.feasibility_check.map_lead_to_feasibility_check",
+                    frm: frm,
+                });
+            },
+            __("Create")
         );
 
+        // Add "Create Quotation" button
+        if (!frm.is_new()) {
+            frm.page.remove_inner_button(__('Quotation'), 'Create');
+            frm.add_custom_button(__('Quotation'), function() {
+                frappe.model.open_mapped_doc({
+                    method: 'versa_system.versa_system.custom_script.quotation.map_lead_to_quotation',
+                    frm: frm
+                });
+            }, __('Create'));
+        }
     }
 });

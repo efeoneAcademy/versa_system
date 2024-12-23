f907ef972ee09b25003239609393aeb0e6614c2d
anshidakk <anshida41@gmail.com>
2024-12-23 10:38:58 +0530

    feat: Cannot able to change type in Design request after save the doctype

--- versa_system/versa_system/doctype/design_request/design_request.js
+++ versa_system/versa_system/doctype/design_request/design_request.js
@@ -3,17 +3,29 @@
 
 frappe.ui.form.on('Design Request', {
     refresh: function(frm) {
-      if (frm.doc.workflow_state === "Approved" && frm.doc.type === "Mockup Design") {
-        // Add a custom button
-        frm.add_custom_button(__('Go to Lead'), function() {
-            // Check if the 'lead' field exists and has a value
-            if (frm.doc.lead) {
-                // Redirect to the specific Lead document
-                frappe.set_route('Form', 'Lead', frm.doc.lead);
-            } else {
-                frappe.msgprint(__('No Lead is linked to this Design Request.'));
-            }
-        }, __('Create Quatation from Lead')); // Group under "Actions"
-      }
+        // Ensure the 'type' field is read-only if its value is 'Mockup Design'
+        if (frm.doc.type === 'Mockup Design') {
+            frm.set_df_property('type', 'read_only', 1);
+        }
+
+        // Add a custom button when the workflow_state is 'Approved'
+        if (frm.doc.workflow_state === "Approved") {
+            frm.add_custom_button(__('Go to Lead'), function() {
+                if (frm.doc.lead) {
+                    frappe.set_route('Form', 'Lead', frm.doc.lead);
+                } else {
+                    frappe.msgprint(__('No Lead is linked to this Design Request.'));
+                }
+            }, __('Create Quotation from Lead')); // Group under "Actions"
+        }
+    },
+
+    type: function(frm) {
+        // Make the 'type' field read-only if its value is 'Mockup Design'
+        if (frm.doc.type === 'Mockup Design') {
+            frm.set_df_property('type', 'read_only', 1);
+        } else {
+            frm.set_df_property('type', 'read_only', 0);
+        }
     }
 });
